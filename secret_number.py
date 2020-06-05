f02a72eeb8367958c261f9a11592522208762c68
jeichel09 <dunyto@gmail.com>
2020-06-05 14:53:57 +0200

    Hausaufgabe 8.2 - Secret number

---
+++ secret_number.py
@@ -0,0 +1,6 @@
+secret = 13
+guess = int(input('Make a guess: '))
+if guess == secret:
+    print('Congratulations! You guessed correctly!')
+else:
+    print('Sorry! That was not the secret number.')
