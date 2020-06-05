5fa45bb1d699936aeba1c7e3301271a6579ff376
jeichel09 <dunyto@gmail.com>
2020-06-05 14:49:05 +0200

    Hausaufgabe 8.1 - Mood checker

---
+++ my_mood.py
@@ -0,0 +1,14 @@
+mood = input('What mood are you in now?: ')
+
+if mood == 'happy':
+    print('It is great to see you happy!')
+elif mood == 'nervous':
+    print('Take a deep breath 3 times.')
+elif mood == 'sad':
+    print('Don\'t be sad!')
+elif mood == 'excited':
+    print('I am excited too')
+elif mood == 'relaxed':
+    print('Wonderful')
+else:
+    print('I don\'t recognize this mood')
---
+++ sandbox.py
@@ -0,0 +1,6 @@
+my_name = input("Gib deinen Namen ein: ")
+my_last_name = input("Gib deinen Familiennamen ein: ")
+print(my_name + ' ' + my_last_name)
+
+# Kommentare
+
